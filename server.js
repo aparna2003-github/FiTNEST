befb9b471f7a7128a02a573212e28be9ec7673b8
Aparna Rajesh <aparnarajesh4005@gmail.com>
2025-10-05 13:22:19 +0530

    change7

---
+++ server.js
@@ -0,0 +1,14 @@
+const jsonServer = require("json-server");
+const cors = require("cors");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(cors());       // enable CORS
+server.use(middlewares);
+server.use(router);
+
+server.listen(process.env.PORT || 5000, () => {
+  console.log("JSON Server running with CORS");
+});
